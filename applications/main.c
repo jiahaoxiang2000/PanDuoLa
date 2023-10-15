7420699daf11a4ab1f88771ae2725b1159ea4174
Xiang JiaHao <simple.xjh@qq.com>
2023-10-15 20:32:53 +0800

    Add cloud module to project
    Added support for 'cloud' to the user project, to enable cloud related functionalities. The makefile (`CMakeLists.txt`) and the source code (`applications/main.c`) have been updated with the new `cloud` module. Along with the addition of the 'cloud' folder which contains 'tencent_cloud.c'  that connects the project with the Tencent cloud. In `tencent_cloud.c`, The Tencent cloud related functionalities, mostly MQTT services, are added for operations on the cloud. Also, new properties and events for the Tencent cloud were set-up in 'data_config.c'.
    The update also included the organization of other files in the project for cleaner code.

--- applications/main.c
+++ applications/main.c
@@ -25,10 +25,6 @@ int main(void) {
     lcd_show_string(10, 69, 32, "Hello, Xjh!");
 
     while (1) {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
         rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
-
     }
 }
