49903c93891057a77e0ca43cd3e5eb07fc6c6de6
Jiahao Xiang <934622645@qq.com>
2024-03-02 22:27:27 +0800

    get the sys cycles by SysTick

--- applications/qarmav2.c
+++ applications/qarmav2.c
@@ -1,8 +1,32 @@
 #include "qarmav2.h"
 #include <rtthread.h>
+#include <stm32l4xx.h>
 
-static void qarma_cipher(int argc, char *argv[]){
-    rt_kprintf("qarma cipher\n");
+static uint32_t a;
+
+#define qarmav2()                       \
+    {                                   \
+        asm volatile("add r0, r0, #1"); \
+    }
+
+static void qarma_cipher(int argc, char *argv[])
+{
+
+    uint32_t start, end, elapsed;
+    qarmav2();
+    qarmav2();
+    qarmav2();
+    qarmav2();
+    start = SysTick->VAL;
+
+    // Your code Start
+    a += 1;
+    // Your code End
+    end = SysTick->VAL;
+
+    elapsed = start > end ? (start - end) : (start + (SysTick->LOAD - end));
+
+    rt_kprintf("Execution time: %lu cycles\n", elapsed);
 }
 
 MSH_CMD_EXPORT_ALIAS(qarma_cipher, qarma, qarma cipher);
